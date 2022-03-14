553708f7fa0a220169bf7d369c977c723290e13e
Justin Conger <jhconger@GMAIL.COM>
2022-03-14 12:31:47 -0500

    first push

---
+++ src/test/java/jUnitTests.java
@@ -0,0 +1,52 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class jUnitTests {
+
+
+       @Test
+        public void testIfNameIsKenneth(){
+           String expected = "Kenneth";
+           String actual = "Kenneth";
+
+           assertEquals(expected, actual);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
